1221b32981ec451baf7fac19aa61194ab3e21398
Michael Royal <45159366+mikeroyal@users.noreply.github.com>
2023-03-17 01:16:31 -0700

    Updated file.

--- Getting Started with Photogrammetry.py
+++ Getting Started with Photogrammetry.py
@@ -1,1 +1,10 @@
 Code samples & snippets coming soon!
+
+# Importing your 3D AR scans to your workspace
+# Import libraries
+import arcpy
+from arcpy import env
+
+arcpy.CheckOutExtension("3D")
+env.workspace = "C:/data"
+arcpy.Import3DFiles_3d("AddAR.skp", "Test.gdb/AddAR", False, "", False)
